6d374f86c8fb249a5cdd4337053bdaf3f7c690f6
Yuriy Yefremov <megadethov@gmail.com>
2017-02-16 15:44:20 +0200

    start project

---
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava;
+
+/**
+ * Created by mega
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Topjava Webinar!");
+    }
+}
