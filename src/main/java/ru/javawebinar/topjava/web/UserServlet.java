0a27b373471e98d38cd665ff640b124076354202
Yuriy Yefremov <megadethov@gmail.com>
2017-02-17 07:56:20 +0200

    add servlet and jsp/html

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mega
+ */
+public class UserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/userList.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mega
+  Date: 17.02.2017
+  Time: 7:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>User list</title>
+</head>
+<body>
+<h2>User list</h2>
+</body>
+</html>
