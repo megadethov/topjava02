87270b252caa9127135348406b3860f6c6c3ccee
Yuriy Yefremov <megadethov@gmail.com>
2017-02-17 22:58:24 +0200

    logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.LoggerWrapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +12,11 @@ import java.io.IOException;
  * Created by mega
  */
 public class UserServlet extends HttpServlet {
+private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        LOG.debug("redirect to userList.jsp");
         response.sendRedirect("userList.jsp");
     }
 }
