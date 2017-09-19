4e5285a89a2baff290e35d4ebe7a72e874c0ed3f
Yuriy Yefremov <megadethov@gmail.com>
2017-09-19 10:28:27 +0300

    Add HSQLDB support Add Tomcat pool Add maven and Spring profiles (hsqldb/postgres)

--- src/main/resources/db/initDB_hsql.sql
+++ src/main/resources/db/initDB_hsql.sql
@@ -12,8 +12,7 @@ CREATE TABLE users
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   registered TIMESTAMP DEFAULT now(),
-  enabled BOOLEAN DEFAULT TRUE,
-  calories_per_day INTEGER DEFAULT 2000 NOT NULL
+  enabled BOOLEAN DEFAULT TRUE
 );
 CREATE UNIQUE INDEX user_email_key ON USERS ( email );
 
