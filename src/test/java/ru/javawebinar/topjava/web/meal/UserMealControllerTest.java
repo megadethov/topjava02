917f524fca17f7ae4cc1413347da9260da6a9afb
Yuriy Yefremov <megadethov@gmail.com>
2017-09-20 14:54:51 +0300

    Add meal and resource controller test

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.web.WebTest;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL1;
+import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
+import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.Profiles.HSQLDB;
+
+@ActiveProfiles({HSQLDB, DATAJPA})
+public class UserMealControllerTest extends WebTest {
+
+    @Test
+    public void testMealList() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("mealList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
+                .andExpect(model().attribute("mealList", hasSize(4)))
+                .andExpect(model().attribute("mealList", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL1_ID)),
+                                hasProperty("description", is(MEAL1.getDescription()))
+                        )
+                )));
+    }
+}
