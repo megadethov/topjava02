3cd76e4e2d07b92fc7ab3932291024dc936cdd9c
Yuriy Yefremov <megadethov@gmail.com>
2017-09-20 15:43:56 +0300

    Correct BaseEntiry

--- src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -55,4 +55,11 @@ public class BaseEntity {
         return (id == null) ? 0 : id;
     }
 
+    public void update(int id) {
+        if (isNew()) {
+            setId(id);
+        } else if (id != getId()) {
+            throw LOG.getIllegalStateException(this + " has id differed from " + id);
+        }
+    }
 }
