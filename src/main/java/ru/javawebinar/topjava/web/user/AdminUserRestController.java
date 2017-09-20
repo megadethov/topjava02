598b55ffc1f5482c7a5923db653bcd0137ccda28
Yuriy Yefremov <megadethov@gmail.com>
2017-09-20 10:57:08 +0300

    Add AdminUserRestController

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUserRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUserRestController.java
@@ -1,51 +1,62 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.LoggerWrapper;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
 
+import java.net.URI;
 import java.util.List;
 
-/**
- * Created by mega
- */
-@Controller
+@RestController
+@RequestMapping("/rest/admin/users")
 public class AdminUserRestController {
-    private static final LoggerWrapper LOG = LoggerWrapper.get(AdminUserRestController.class);
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserRestController.class);
 
     @Autowired
-    private UserService service;
+    private UserHelper helper;
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAll() {
-        LOG.info("getAll");
-        return service.getAll();
+        return helper.getAll();
     }
 
-    public User get(int id) {
-        LOG.info("get " + id);
-        return service.get(id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@PathVariable("id") int id) {
+        return helper.get(id);
     }
 
-    public User create(User user) {
-        LOG.info("create " + user);
-        return service.save(user);
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User created = helper.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/rest/admin/users/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uriOfNewResource);
+
+        return new ResponseEntity<>(created, httpHeaders, HttpStatus.CREATED);
     }
 
-    public void delete(int id) {
-        LOG.info("delete " + id);
-        service.delete(id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id) {
+        helper.delete(id);
     }
 
-    public void update(User user) {
-        LOG.info("update " + user);
-        service.update(user);
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user) {
+        helper.update(user);
     }
 
-    public User getByMail(String email) {
-        LOG.info("getByEmail " + email);
-        return service.getByEmail(email);
+    @RequestMapping(value = "/by", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getByMail(@RequestParam("email") String email) {
+        return helper.getByMail(email);
     }
-
 }
